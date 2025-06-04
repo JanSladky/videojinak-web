78adb8cb2b52c9c692f9b65733fdc38f90493399
Jan Sladký <sladky.honza@gmail.com>
2025-06-04 08:42:17 +0200

    Fix: pridani do next.config.js ještě vimeo player

--- next.config.ts
+++ next.config.ts
@@ -13,8 +13,8 @@ const nextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
-              script-src 'self' https://www.google.com https://www.gstatic.com https://www.recaptcha.net 'unsafe-inline';
-              frame-src https://www.youtube.com https://www.google.com https://www.recaptcha.net;
+              script-src 'self' https://www.youtube.com https://www.google.com https://player.vimeo.com https://www.gstatic.com https://www.recaptcha.net 'unsafe-inline';
+              frame-src https://www.youtube.com https://player.vimeo.com https://www.google.com https://www.recaptcha.net;
               style-src 'self' 'unsafe-inline';
               img-src 'self' data: https://www.gstatic.com https://www.google.com;
               connect-src 'self' https://www.google.com https://www.recaptcha.net;
