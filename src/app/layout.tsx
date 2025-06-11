a1f635dfd7e62ef9f0993513f629a9c914fcfdd7
Jan Sladký <sladky.honza@gmail.com>
2025-06-11 16:46:37 +0200

    Úprava metadat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,44 @@
-import './globals.css';
-import { ReactNode } from 'react';
-import ClientLayout from './ClientLayout';
+import "./globals.css";
+import { ReactNode } from "react";
+import ClientLayout from "./ClientLayout";
 
 export const metadata = {
-  title: 'Videojinak.cz',
-  description: 'Specializujeme se na promo, klipy, komerční i svatební videa. Nejraději děláme akčnější videa, a když do nich můžeme promítnout i srandu, pak jedině super.',
-   icons: {
+  title: "Videojinak.cz – Firemní, svatební a reklamní videa",
+  description:
+    "Točíme firemní videa, svatební videoklipy, produktové reklamy i zábavné promo spoty. Kreativní zpracování, profesionální kvalita, cit pro detail a nápaditý střih – to je Videojinak.cz.",
+  keywords: [
+    "firemní video",
+    "svatební video",
+    "reklamní spot",
+    "promo video",
+    "produktové video",
+    "videoprodukce",
+    "videotvorba",
+    "video pro firmy",
+    "svatba video",
+    "komerční video",
+    "videoklip",
+    "Videojinak.cz",
+  ],
+  authors: [{ name: "Jan Sladký", url: "https://www.videojinak.cz" }],
+  creator: "Videojinak.cz",
+  metadataBase: new URL("https://www.videojinak.cz"),
+  openGraph: {
+    title: "Videojinak.cz – Tvoříme videa s nápadem",
+    description: "Kreativní videoprodukce: svatební klipy, firemní prezentace, reklamní a produktová videa. Natáčíme originálně a s chutí!",
+    url: "https://www.videojinak.cz",
+    siteName: "Videojinak.cz",
+    images: [
+      {
+        url: "/uvodni_foto.png", // přidej si vlastní obrázek do /public/images/
+        width: 1200,
+        height: 630,
+        alt: "Videojinak.cz – ukázka tvorby",
+      },
+    ],
+    type: "website",
+  },
+  icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon.ico",
     apple: "/apple-touch-icon.png",
